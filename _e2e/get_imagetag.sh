a989f9b1677a8562fa007d526505445d4a777781
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2024-01-16 11:55:13 +0100

    Add option to retrieve full image name for project

--- _e2e/get_imagetag.sh
+++ _e2e/get_imagetag.sh
@@ -12,11 +12,24 @@ function check_equal() {
 
 # Run the e2e tests
 
-git_dir=$(mktemp -d)
+tmp_dir=$(mktemp -d)
+project="e2e"
+git_dir="$tmp_dir/$project"
+mkdir "$git_dir"
+cd "$git_dir"
 git init "$git_dir"
 
 mkdir -p "$git_dir/rootfs"
-cd $git_dir
+
+# Add ciux config file
+file="$git_dir/.ciux"
+
+cat > "$file" <<EOF
+apiVersion: v1alpha1
+registry: test_url/test_org
+EOF
+git add "$file"
+git commit -m "Add $file"
 
 # Add a file to the rootfs
 file="$git_dir/rootfs/hello.txt"
@@ -28,7 +41,7 @@ git_tag="v1.0.0"
 git tag -a "$git_tag" -m "Release $git_tag"
 
 ver=$(ciux get imagetag "$git_dir")
-check_equal "$ver" "$git_tag"
+check_equal "$git_tag" "$ver"
 
 file="$git_dir/hello2.txt"
 echo "Hello World" > "$file"
@@ -36,12 +49,17 @@ git add "$file"
 git commit -m "Add $file"
 
 ver=$(ciux get imagetag "$git_dir")
-check_equal "$ver" "$git_tag"
+check_equal "$git_tag" "$ver"
 
 file="$git_dir/rootfs/hello3.txt"
 echo "Hello World" > "$file"
 git add "$file"
 git commit -m "Add $file"
 
-ver=$(ciux get imagetag "$git_dir")
-check_equal "$ver" "$git_tag-2-g$(git rev-parse --short HEAD)"
+img_tag=$(ciux get imagetag "$git_dir")
+expected_img_tag="$git_tag-2-g$(git rev-parse --short HEAD)"
+check_equal "$expected_img_tag" "$img_tag"
+
+img_url=$(ciux get imagetag --full "$git_dir")
+expected_img_url="test_url/test_org/$project:$expected_img_tag"
+check_equal "$expected_img_url" "$img_url"
--- cmd/get_imagetag.go
+++ cmd/get_imagetag.go
@@ -11,6 +11,7 @@ import (
 )
 
 var pathes []string
+var full bool
 
 // imageTagCmd represents the revision command
 var imageTagCmd = &cobra.Command{
@@ -23,15 +24,33 @@ var imageTagCmd = &cobra.Command{
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		repositoryPath := args[0]
-		gitMeta, err := internal.NewGit(repositoryPath)
+		var project internal.Project
+		var err error
+		var gitMain *internal.Git
+
+		if full {
+			repositoryPath := args[0]
+			project, err = internal.NewProject(repositoryPath, branch, labelSelector)
+			internal.FailOnError(err)
+			gitMain = project.GitMain
+		} else {
+			gitMain, err = internal.NewGit(repositoryPath)
+			internal.FailOnError(err)
+		}
+		head, err := gitMain.Repository.Head()
 		internal.FailOnError(err)
-		head, err := gitMeta.Repository.Head()
+		commit, err := internal.FindCodeChange(gitMain.Repository, head.Hash(), pathes)
 		internal.FailOnError(err)
-		commit, err := internal.FindCodeChange(gitMeta.Repository, head.Hash(), pathes)
+		rev, err := gitMain.GetRevision(commit.Hash)
 		internal.FailOnError(err)
-		rev, err := gitMeta.GetRevision(commit.Hash)
-		internal.FailOnError(err)
-		fmt.Printf("%s\n", rev.GetVersion())
+		if full {
+			name, err := gitMain.GetName()
+			internal.FailOnError(err)
+			imageUrl := fmt.Sprintf("%s/%s:%s", project.ImageRegistry, name, rev.GetVersion())
+			fmt.Printf("%s\n", imageUrl)
+		} else {
+			fmt.Printf("%s\n", rev.GetVersion())
+		}
 	},
 }
 
@@ -39,6 +58,7 @@ func init() {
 	getCmd.AddCommand(imageTagCmd)
 
 	imageTagCmd.Flags().StringSliceVarP(&pathes, "pathes", "p", []string{"rootfs"}, "Relative pathes to source code used to build the container image")
+	imageTagCmd.Flags().BoolVarP(&full, "full", "f", false, "Display full image name")
 }
 
 // Create a golang function which returns the revision of a git repository
